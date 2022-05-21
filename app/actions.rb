ea77c769acb8fa358db50a744436439dd72801c3
ShiranuiSecond <dominik9ice@hotmail.com>
2022-05-21 22:06:31 +0000

    w5d1

--- app/actions.rb
+++ app/actions.rb
@@ -1,12 +1,55 @@
-def humanized_time_ago(minute_num)
-  if minute_num >= 60
-    "#{minute_num / 60} hours ago"
-  else
-    "#{minute_num} minutes ago"
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
   end
 end
 
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
+  @current_user = User.find_by(id: session[:user_id])
   erb(:index)
+end
+
+get '/signup' do     # if a user navigates to the path "/signup",
+  @user = User.new   # setup empty @user object
+  erb(:signup)       # render "app/views/signup.erb"
+end
+
+post '/signup' do
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
+  if @user.save
+    redirect to('/login')
+  else
+    erb(:signup)
+  end
+end
+
+get '/login' do    # when a GET request comes into /login
+  erb(:login)      # render app/views/login.erb
+end
+
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)  
+
+  if user && user.password == password
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
 end
